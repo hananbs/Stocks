5dd2ffef0d4c799c691daf33f2150429f3e680e6
sharon77242 <sharon10vp614@gmail.com>
2015-08-20 22:35:58 +0300

    Added more programs from net + modified square rogram

---
+++ YahooFinanceCSV/Jarloo.YahooFinanceCSV/Jarloo.YahooFinanceCSV/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using jarloo;
+
+namespace Jarloo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string csvData;
+
+            using (WebClient web = new WebClient())
+            {
+                csvData = web.DownloadString("http://finance.yahoo.com/d/quotes.csv?s=AAPL+GOOG+MSFT&f=snbaopl1");
+            }
+
+            List<Price> prices = YahooFinance.Parse(csvData);
+
+            foreach (Price price in prices)
+            {
+                Console.WriteLine(string.Format("{0} ({1})  Bid:{2} Offer:{3} Last:{4} Open: {5} PreviousClose:{6}",price.Name,price.Symbol,price.Bid,price.Ask,price.Last,price.Open,price.PreviousClose));
+            }
+
+            Console.Read();
+
+        }
+    }
+}
